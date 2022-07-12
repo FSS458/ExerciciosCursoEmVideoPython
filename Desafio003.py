5867f60232f393d919e2bb6bc4f059ce0089a079
Felipe Souza Da Silva <78595727+FSS458@users.noreply.github.com>
2022-07-12 14:57:10 -0300

    Update Desafio003.py

--- Desafio003.py
+++ Desafio003.py
@@ -1,3 +1,5 @@
+# Crie um programa que leia dois números e mostre a soma entre eles.
+
 num1 = int(input("Qual é o primeiro número? "))
 num2 = int(input("Qual é o segundo número? "))
 soma = num1 + num2
