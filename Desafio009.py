6a0de53e326bb0cc27e92de7366e604f204cb2b1
Felipe Souza Da Silva <78595727+FSS458@users.noreply.github.com>
2022-07-12 15:40:49 -0300

    Add files via upload

---
+++ Desafio009.py
@@ -0,0 +1,18 @@
+# Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
+
+num = float(input("Qual número você deseja verificar a tabuada? "))
+
+print(f""" O número que você forneceu é {num}
+    1 X {num} = {num}
+    2 x {num} = {num * 2}
+    3 X {num} = {num * 3}
+    4 X {num} = {num * 4}
+    5 X {num} = {num * 5}   
+    6 X {num} = {num * 6}   
+    7 X {num} = {num * 7}   
+    8 X {num} = {num * 8}   
+    9 X {num} = {num * 9}   
+    10 X {num} = {num * 10}   
+    
+    
+""")
