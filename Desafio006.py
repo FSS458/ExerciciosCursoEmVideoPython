c64f54d4c258b026cfaa08985b484fe1e572e3e5
Felipe Souza Da Silva <78595727+FSS458@users.noreply.github.com>
2022-07-12 14:55:46 -0300

    Add files via upload

---
+++ Desafio006.py
@@ -0,0 +1,7 @@
+
+num = int(input("Qual é o número que você deseja revelar? "))
+numDobro = num * 2
+numTriplo = num * 3
+numRaizQuadrada = num ** (1/2)
+
+print(f" O número que você forneceu é {num} \n O dobro desse número é {numDobro} \n O triplo desse número é {numTriplo} \n A raiz quadrada desse número é {numRaizQuadrada}")
