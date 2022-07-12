959d4165afdbed457c65e614297895a35a3beb23
Felipe Souza Da Silva <78595727+FSS458@users.noreply.github.com>
2022-07-12 15:05:12 -0300

    Add files via upload

---
+++ Desafio007.py
@@ -0,0 +1,7 @@
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+
+nota1 = int(input("Qual foi a primeira nota desse aluno? "))
+nota2 = int(input("Qual foi a segunda nota desse aluno? "))
+media = (nota1 + nota2) / 2
+
+print(f"A média desse aluno foi de {media}")
