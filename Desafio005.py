d39351fd87f49c06a04356bd74f8d047a1b4f7a8
Felipe Souza Da Silva <78595727+FSS458@users.noreply.github.com>
2022-07-12 14:39:55 -0300

    Add files via upload

---
+++ Desafio005.py
@@ -0,0 +1,4 @@
+num = int(input("Qual é o número que você deseja revelar? "))
+numSucessor = num + 1
+numAntecessor = num - 1
+print(f"O seu número é {num} o antecessor dele é {numAntecessor} e sucessor dele é {numSucessor}")
