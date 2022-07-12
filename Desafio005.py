f0886dda70dc764ead9917fc5de2b429c795489f
Felipe Souza Da Silva <78595727+FSS458@users.noreply.github.com>
2022-07-12 14:57:52 -0300

    Update Desafio005.py

--- Desafio005.py
+++ Desafio005.py
@@ -1,3 +1,5 @@
+# Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+
 num = int(input("Qual é o número que você deseja revelar? "))
 numSucessor = num + 1
 numAntecessor = num - 1
