b86c801f50f764c942a8723850a38f258d22ae34
Felipe Souza Da Silva <78595727+FSS458@users.noreply.github.com>
2023-01-16 11:15:09 -0300

    Update Desafio012.py

--- Desafio012.py
+++ Desafio012.py
@@ -1,3 +1,4 @@
+# Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 preco = float(input("Qual seria o preço do produto que você gostaria de avaliar? "))
 
 precoDesconto = preco * 95/100
