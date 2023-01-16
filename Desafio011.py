af44fb4668df365b0e97f93790534c80b3e77a6c
Felipe Souza Da Silva <78595727+FSS458@users.noreply.github.com>
2023-01-16 11:15:56 -0300

    Update Desafio011.py

--- Desafio011.py
+++ Desafio011.py
@@ -1,4 +1,5 @@
 # Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2 metros quadrados.
+
 altura = float(input("Qual é a altura da sua parede em metros? "))
 largura = float(input("Qual é a largura da sua parede em metros? "))
 
