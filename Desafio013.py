21bc9382f43372ca61a2ba92b632ada56e2474c5
Felipe Souza Da Silva <78595727+FSS458@users.noreply.github.com>
2023-01-16 11:16:35 -0300

    Update Desafio013.py

--- Desafio013.py
+++ Desafio013.py
@@ -1,3 +1,5 @@
+# Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+
 salario = float(input("Qual é o valor do seu salário atualmente? "))
 
 aumentoSalario = salario * 115/100
