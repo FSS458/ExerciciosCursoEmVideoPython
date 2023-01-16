513b60e6ecea2c0691660d537485248968fc2db4
Felipe Souza Da Silva <78595727+FSS458@users.noreply.github.com>
2023-01-16 11:17:18 -0300

    Update Desafio015.py

--- Desafio015.py
+++ Desafio015.py
@@ -1,3 +1,5 @@
+# Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.
+
 dias = int(input("Por quantos dias esse carro foi alugado? "))
 quilometragem = float(input("Quantos km esse caro percorreu? "))
 
